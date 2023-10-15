p0deje/Maccy

commit c171ad8ff15b422e4f07584bd5e80d36bbb7de96

Fix key handling on macOS 14
Related to https://github.com/feedback-assistant/reports/issues/412

Maccy.xcodeproj/project.pbxproj
@@ -78,6 +78,7 @@
 		DABDE97F2974706C005B32E9 /* KeyboardLayout.swift in Sources */ = {isa = PBXBuildFile; fileRef = DABDE97E2974706C005B32E9 /* KeyboardLayout.swift */; };
 		DAC14124232367B200FCFA30 /* Search.swift in Sources */ = {isa = PBXBuildFile; fileRef = DAC14123232367B200FCFA30 /* Search.swift */; };
 		DAC6CE9126E1AC19002B70B8 /* LaunchAtLogin in Frameworks */ = {isa = PBXBuildFile; productRef = DAC6CE9026E1AC19002B70B8 /* LaunchAtLogin */; };
+		DAC7AFAF2A801C92006081BB /* RunLoopLocalEventMonitor.swift in Sources */ = {isa = PBXBuildFile; fileRef = DAC7AFAE2A801C92006081BB /* RunLoopLocalEventMonitor.swift */; };
 		DAC929D7297A0E8B00814F19 /* NSPasteboard.PasteboardType+Types.swift in Sources */ = {isa = PBXBuildFile; fileRef = DAC929D6297A0E8B00814F19 /* NSPasteboard.PasteboardType+Types.swift */; };
 		DAD0862D24545C66002AFAEC /* Storage.xcdatamodeld in Sources */ = {isa = PBXBuildFile; fileRef = DAB65DFB2440AD63000AECA8 /* Storage.xcdatamodeld */; };
 		DAD20C7724E55CD200E96247 /* org.sparkle-project.Downloader.xpc in Resources */ = {isa = PBXBuildFile; fileRef = DAD20C7224E55CD100E96247 /* org.sparkle-project.Downloader.xpc */; };
@@ -314,6 +315,7 @@
 		DAB8CE4124E368F200A2500E /* container-migration.plist */ = {isa = PBXFileReference; lastKnownFileType = text.plist.xml; path = "container-migration.plist"; sourceTree = "<group>"; };
 		DABDE97E2974706C005B32E9 /* KeyboardLayout.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = KeyboardLayout.swift; sourceTree = "<group>"; };
 		DAC14123232367B200FCFA30 /* Search.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = Search.swift; sourceTree = "<group>"; };
+		DAC7AFAE2A801C92006081BB /* RunLoopLocalEventMonitor.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = RunLoopLocalEventMonitor.swift; sourceTree = "<group>"; };
 		DAC929D6297A0E8B00814F19 /* NSPasteboard.PasteboardType+Types.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "NSPasteboard.PasteboardType+Types.swift"; sourceTree = "<group>"; };
 		DAD20C7224E55CD100E96247 /* org.sparkle-project.Downloader.xpc */ = {isa = PBXFileReference; lastKnownFileType = "wrapper.xpc-service"; name = "org.sparkle-project.Downloader.xpc"; path = "Sparkle/org.sparkle-project.Downloader.xpc"; sourceTree = "<group>"; };
 		DAD20C7324E55CD100E96247 /* org.sparkle-project.InstallerStatus.xpc */ = {isa = PBXFileReference; lastKnownFileType = "wrapper.xpc-service"; name = "org.sparkle-project.InstallerStatus.xpc"; path = "Sparkle/org.sparkle-project.InstallerStatus.xpc"; sourceTree = "<group>"; };
@@ -587,6 +589,7 @@
 				DA49EE7428B59468002752E0 /* NSRect+Centered.swift */,
 				DA49EE7828B594DC002752E0 /* NSRunningApplication+WindowFrame.swift */,
 				DA6491AF29ABCF2400837D93 /* NSScreen+ForPopup.swift */,
+				DAC7AFAE2A801C92006081BB /* RunLoopLocalEventMonitor.swift */,
 				DAC14123232367B200FCFA30 /* Search.swift */,
 				DA696BCD240177E800DE80CF /* Sorter.swift */,
 				DAFE2DD9268A521A00990986 /* String+Shortened.swift */,
@@ -864,6 +867,7 @@
 				DA57C64E249E2A550032707A /* MenuFooter.swift in Sources */,
 				DAB65DFF2440AE29000AECA8 /* CoreDataManager.swift in Sources */,
 				DA2B0AF2234201E500EFAD72 /* UserDefaults+Configuration.swift in Sources */,
+				DAC7AFAF2A801C92006081BB /* RunLoopLocalEventMonitor.swift in Sources */,
 				DA0EF1881E444B2A00E58577 /* Clipboard.swift in Sources */,
 				DA181170247B405B00066D55 /* GeneralPreferenceViewController.swift in Sources */,
 				DAAEB196219694AE00A7883C /* About.swift in Sources */,

Maccy/MenuHeader/MenuHeaderView.swift
@@ -10,10 +10,6 @@ class MenuHeaderView: NSView, NSSearchFieldDelegate {
   @IBOutlet weak var horizontalRightPadding: NSLayoutConstraint!
   @IBOutlet weak var titleAndSearchSpacing: NSLayoutConstraint!
 
-  private let eventSpecs = [
-    EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventRawKeyDown)),
-    EventTypeSpec(eventClass: OSType(kEventClassKeyboard), eventKind: UInt32(kEventRawKeyRepeat))
-  ]
   private let macOSXLeftPadding: CGFloat = 20.0
   private let macOSXRightPadding: CGFloat = 10.0
   private let searchThrottler = Throttler(minimumDelay: 0.4)
@@ -21,7 +17,14 @@ class MenuHeaderView: NSView, NSSearchFieldDelegate {
   private var characterPickerVisible: Bool {
     NSApp.windows.filter({ $0.isVisible }).map({ $0.className }).contains("NSPanelViewBridge")
   }
-  private var eventHandler: EventHandlerRef?
+
+  private lazy var eventMonitor = RunLoopLocalEventMonitor(events: .keyDown, runLoopMode: .eventTracking) { event in
+    if self.processInterceptedEvent(event) {
+      return nil
+    } else {
+      return event
+    }
+  }
 
   private lazy var customMenu: Menu? = self.enclosingMenuItem?.menu as? Menu
   private lazy var headerHeight = UserDefaults.standard.hideSearch ? 1 : 28
@@ -55,30 +58,9 @@ class MenuHeaderView: NSView, NSSearchFieldDelegate {
     super.viewDidMoveToWindow()
 
     if window != nil {
-      if let dispatcher = GetEventDispatcherTarget() {
-        // Create pointer to our event processer.
-        let eventProcessorPointer = UnsafeMutablePointer<Any>.allocate(capacity: 1)
-        eventProcessorPointer.initialize(to: processInterceptedEventRef)
-
-        let eventHandlerCallback: EventHandlerUPP = { _, eventRef, userData in
-          guard let event = eventRef else { return noErr }
-          guard let callbackPointer = userData else { return noErr }
-
-          // Call our event processor from pointer.
-          let eventProcessPointer = UnsafeMutablePointer<(EventRef) -> (Bool)>(OpaquePointer(callbackPointer))
-          let eventProcessed = eventProcessPointer.pointee(event)
-
-          if eventProcessed {
-            return noErr
-          } else {
-            return OSStatus(Carbon.eventNotHandledErr)
-          }
-        }
-
-        InstallEventHandler(dispatcher, eventHandlerCallback, 2, eventSpecs, eventProcessorPointer, &eventHandler)
-      }
+      eventMonitor.start()
     } else {
-      RemoveEventHandler(eventHandler)
+      eventMonitor.stop()
       DispatchQueue.main.async {
         self.setQuery("")
         self.queryField.refusesFirstResponder = true

Maccy/Menu.swift
@@ -90,6 +90,7 @@ class Menu: NSMenu, NSMenuDelegate {
     offloadCurrentPreview()
   }
 
+  // swiftlint:disable function_body_length
   func menu(_ menu: NSMenu, willHighlight item: NSMenuItem?) {
     offloadCurrentPreview()
 
@@ -116,7 +117,8 @@ class Menu: NSMenu, NSMenuDelegate {
       if let previewView = indexedItem.previewMenuItem.view,
          let previewWindow = previewView.window,
          let windowContentView = previewWindow.contentView {
-        // Check if the preview item is non-obstructed (which can e.g. happen is scrollable and the scroll arrow overlaps the item)
+        // Check if the preview item is non-obstructed
+        // (which can e.g. happen is scrollable and the scroll arrow overlaps the item)
         guard previewView.superview?.superview != nil else { return }
 
         previewThrottle.minimumDelay = Menu.subsequentPreviewDelay
@@ -150,7 +152,10 @@ class Menu: NSMenu, NSMenuDelegate {
         )
 
         let heightOfVisibleMenuItem = abs(topPoint.y - bottomPoint.y)
-        let boundsOfVisibleMenuItem = NSRect(origin: bottomPoint, size: NSSize(width: previewView.bounds.width, height: heightOfVisibleMenuItem))
+        let boundsOfVisibleMenuItem = NSRect(
+          origin: bottomPoint,
+          size: NSSize(width: previewView.bounds.width, height: heightOfVisibleMenuItem)
+        )
 
         previewPopover?.show(
           relativeTo: boundsOfVisibleMenuItem,
@@ -160,14 +165,19 @@ class Menu: NSMenu, NSMenuDelegate {
 
         if let popoverWindow = previewPopover?.contentViewController?.view.window {
           if popoverWindow.frame.minX < previewWindow.frame.minX {
-            popoverWindow.setFrameOrigin(NSPoint(x: popoverWindow.frame.minX - Menu.popoverGap, y: popoverWindow.frame.minY))
+            popoverWindow.setFrameOrigin(
+              NSPoint(x: popoverWindow.frame.minX - Menu.popoverGap, y: popoverWindow.frame.minY)
+            )
           } else {
-            popoverWindow.setFrameOrigin(NSPoint(x: popoverWindow.frame.minX + Menu.popoverGap, y: popoverWindow.frame.minY))
+            popoverWindow.setFrameOrigin(
+              NSPoint(x: popoverWindow.frame.minX + Menu.popoverGap, y: popoverWindow.frame.minY)
+            )
           }
         }
       }
     }
   }
+  // swiftlint:enable function_body_length
 
   func buildItems() {
     clearAll()
@@ -244,8 +254,11 @@ class Menu: NSMenu, NSMenuDelegate {
 
     // Ensure that pinned items are visible after search is cleared.
     if filter.isEmpty {
-      results.append(contentsOf: indexedItems.filter({ $0.item.pin != nil })
-                                             .map({ Search.SearchResult(score: nil, object: $0, titleMatches: []) }))
+      results.append(
+        contentsOf: indexedItems
+          .filter({ $0.item.pin != nil })
+          .map({ Search.SearchResult(score: nil, object: $0, titleMatches: []) })
+      )
     }
 
     // First, remove items that don't match search.

Maccy/RunLoopLocalEventMonitor.swift
@@ -1,0 +1,60 @@
+import AppKit
+
+// Credits to https://github.com/sindresorhus/KeyboardShortcuts/commit/8b1a9ce78c2f35c8a55dcc95897573abd2cc4f6e.
+final class RunLoopLocalEventMonitor {
+  private let runLoopMode: RunLoop.Mode
+  private let callback: (NSEvent) -> NSEvent?
+  private let observer: CFRunLoopObserver
+
+  init(
+    events: NSEvent.EventTypeMask,
+    runLoopMode: RunLoop.Mode,
+    callback: @escaping (NSEvent) -> NSEvent?
+  ) {
+    self.runLoopMode = runLoopMode
+    self.callback = callback
+
+    self.observer = CFRunLoopObserverCreateWithHandler(nil, CFRunLoopActivity.beforeSources.rawValue, true, 0) { _, _ in
+      // Pull all events from the queue and handle the ones matching the given types.
+      // Non-matching events are left untouched, maintaining their order in the queue.
+      var eventsToHandle = [NSEvent]()
+
+      // Retrieve all events from the event queue to preserve their order (instead of using the `matching` parameter).
+      while let eventToHandle = NSApp.nextEvent(matching: .any, until: nil, inMode: .default, dequeue: true) {
+        eventsToHandle.append(eventToHandle)
+      }
+
+      // Iterate over the gathered events, instead of doing it directly in the `while` loop,
+      // to avoid potential infinite loops caused by re-retrieving undiscarded events.
+      for eventToHandle in eventsToHandle {
+        var handledEvent: NSEvent?
+
+        if !events.contains(NSEvent.EventTypeMask(rawValue: 1 << eventToHandle.type.rawValue)) {
+          handledEvent = eventToHandle
+        } else if let callbackEvent = callback(eventToHandle) {
+          handledEvent = callbackEvent
+        }
+
+        guard let handledEvent else {
+          continue
+        }
+
+        NSApp.postEvent(handledEvent, atStart: false)
+      }
+    }
+  }
+
+  deinit {
+    stop()
+  }
+
+  @discardableResult
+  func start() -> Self {
+    CFRunLoopAddObserver(RunLoop.current.getCFRunLoop(), observer, CFRunLoopMode(runLoopMode.rawValue as CFString))
+    return self
+  }
+
+  func stop() {
+    CFRunLoopRemoveObserver(RunLoop.current.getCFRunLoop(), observer, CFRunLoopMode(runLoopMode.rawValue as CFString))
+  }
+}

gityeop/FlowClip

commit 6ba95258a6cd185e5f4c08f1b05facb1cf21546d

Allow to command-select pins with "v" key
This is likely a regression introduced in c171ad8ff15b422e4f07584bd5e80d36bbb7de96
which intercepts more key presses now.

Fixes #624

Maccy.xcodeproj/project.pbxproj
@@ -556,8 +556,8 @@
 				DAF65F4424D396D400F3978F /* HistoryMenuItem */,
 				DA68F91F28BD819E007E2F25 /* Intents */,
 				DA196A8D28AB0A3700ADD8A2 /* MenuHeader */,
-				DA181178247D150B00066D55 /* Settings */,
 				DA19F18C2984940B0091F26C /* Preview */,
+				DA181178247D150B00066D55 /* Settings */,
 				DA21AD3C249E85BD003E7C98 /* Sounds */,
 				DA246D601E56C9EA001E40F3 /* Application.xib */,
 				DA6373971E4AB9BB00263391 /* Assets.xcassets */,

Maccy/HistoryItem.swift
@@ -19,6 +19,10 @@ class HistoryItem: NSManagedObject {
     }
   }
 
+  static var pinned: [HistoryItem] {
+    all.filter({ $0.pin != nil })
+  }
+
   static var unpinned: [HistoryItem] {
     all.filter({ $0.pin == nil })
   }

Maccy/MenuHeader/MenuHeaderView.swift
@@ -167,9 +167,13 @@ class MenuHeaderView: NSView, NSSearchFieldDelegate {
       performMenuItemAction(MenuFooter.preferences.rawValue)
       return true
     case .paste:
-      queryField.becomeFirstResponder()
-      queryField.currentEditor()?.paste(nil)
-      return true
+      if HistoryItem.pinned.contains(where: { $0.pin == key.rawValue }) {
+        return false
+      } else {
+        queryField.becomeFirstResponder()
+        queryField.currentEditor()?.paste(nil)
+        return true
+      }
     case .selectCurrentItem:
       customMenu?.select(queryField.stringValue)
       return true