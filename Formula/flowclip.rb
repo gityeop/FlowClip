73e20aeadaa2debcf2146d8f91b24ec478d62c0a
gityeop <freiheit517@icloud.com>
2026-01-22 22:46:44 +0900

    Release: Bump version to 1.0.5 (Fixed Transparency Rendering)

--- Formula/flowclip.rb
+++ Formula/flowclip.rb
@@ -1,8 +1,8 @@
 cask "flowclip" do
-  version "1.0.4"
-  sha256 "faadb94eb68b93fe5f0a08ee81d632b118c1b42a72f4397b1b6f4f7e29324602"
+  version "1.0.5"
+  sha256 "2b0f0581e86545dee33a74d057718de02cab1f30a8c36d9211c4057f27049df9"
 
-  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.4.zip"
+  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.5.zip"
   name "FlowClip"
   desc "Clipboard manager with Queue support (Fork of Maccy)"
   homepage "https://github.com/gityeop/FlowClip"
