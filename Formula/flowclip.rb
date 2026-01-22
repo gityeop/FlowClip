5c4fd0553b3f449bc582e50b4674492a102a8aed
gityeop <freiheit517@icloud.com>
2026-01-22 21:02:13 +0900

    Release: Bump version to 1.0.3 (Fix Icon Black Corners)

--- Formula/flowclip.rb
+++ Formula/flowclip.rb
@@ -1,8 +1,8 @@
 cask "flowclip" do
-  version "1.0.2"
-  sha256 "97b0d463e5cea734c9d67352db4533df36df2d435bc06951d8237661cb75df3e"
+  version "1.0.3"
+  sha256 "a64f8c0866e4440fb170fb56fc3a760b06a379f87bd20e1d109ff51d42d9f4d2"
 
-  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.2.zip"
+  url "https://github.com/gityeop/FlowClip/releases/download/v#{version}/FlowClip_1.0.3.zip"
   name "FlowClip"
   desc "Clipboard manager with Queue support (Fork of Maccy)"
   homepage "https://github.com/gityeop/FlowClip"
